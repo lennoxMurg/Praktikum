193661587b3cf7011d1cf6c0a0753acc20a9b466
Lennox <lennoxmurg@gmail.com>
2026-06-23 09:35:17 +0200

    Aufgabe 2, 3 und 4
    Aufgabe 2 so überarbeitet das es nur mit Conj arbeitet. Aufgabe 3 und 4 implementiert

--- Clojure Code/fwpd/src/fwpd/core.clj
+++ Clojure Code/fwpd/src/fwpd/core.clj
@@ -1,25 +1,3 @@
-(ns fwpd.core)
-(def filename "suspects.csv")
-
-
-(def vamp-keys [:name :glitter-index])
-
-(defn str->int
-  [str]
-  (Integer. str))
-
-(def conversions {:name identity
-                  :glitter-index str->int})
-
-(defn convert
-  [vamp-key value]
-  ((get conversions vamp-key) value))
-
-(defn parse
-  "Convert a CSV into rows of columns"
-  [string]
-  (map #(clojure.string/split % #",")
-       (clojure.string/split string #"\n")))
 
 (defn mapify
   "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
@@ -33,7 +11,7 @@
 
 (defn glitter-filter
   [minimum-glitter records]
-  (filter #(>= (:glitter-index %) minimum-glitter)))
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
 
 
 ;; Exercise 1 : Turn the result of your glitter filter into a list of names
@@ -46,6 +24,40 @@
 ;; Exercise 2 : Write a function, append, which will append a new suspect to your list of suspects
 
 (defn append
-  [vamp-map]
-  (let [csv-line (str (:name vamp-map) "," (:glitter-index vamp-map) "\n")]
-    (spit filename csv-line :append true)))
+  [records name-str glitter-int]
+  (let [new-suspect {:name name-str :glitter-index glitter-int}]
+    (conj records new-suspect)))
+
+;; Exercise 3 : Write a function, validate, which will check that :name and :glitter-index are present when you append
+
+(def validation-rules {:name string? 
+                       :glitter-index integer?})
+
+(defn validate
+  [validations record]
+  (let [name-fn (:name validations)
+        glitter-fn (:glitter-index validations)]
+    (if (and (name-fn (:name record))
+             (glitter-fn (:glitter-index record)))
+      true
+      false)))
+
+;; The append function with the validate function check
+
+(defn append-v2
+  [records name-str glitter-int]
+  (let [new-suspect {:name name-str :glitter-index glitter-int}]
+    (if (validate validation-rules new-suspect)
+      (do
+        (println "Succes - Suspect added to list!")
+        (conj records new-suspect))
+      (do
+        (println "Error - Name needs to be String and glitter-index needs to be int!")
+        records)))) ;; Gibt einfach die alte Liste unverändert zurück
+
+;; Exercise 4 : 
+
+(defn record-to-csv
+  [records]
+  (let [lines (map (fn [r] (str (:name r) "," (:glitter-index r))) records)]
+    (clojure.string/join "\n" lines)))
